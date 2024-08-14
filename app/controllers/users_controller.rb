3a1b44c813f65cc5d2cac265e13a5b8b5cf8c592
nhatminhdao <nhatminhdao@gmail.com>
2024-08-14 09:18:40 +0000

    user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,14 @@ class UsersController < ApplicationController
     render({ :template => "users/index" })
   end
 
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_users = User.where({ :id => the_id })
+
+    @the_user = matching_users.at(0)
+
+    render({ :template => "users/show" })
+  end
   
 end
