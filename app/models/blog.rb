72653f639c35c88d407a6f01499768d990d1a19f
nhatminhdao <nhatminhdao@gmail.com>
2024-08-16 03:43:13 +0000

    add some associations and validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -13,4 +13,11 @@
 #  blogger_id     :integer
 #
 class Blog < ApplicationRecord
+  validates(:content, presence: true)
+  validates(:book_title, presence: true)
+  
+  belongs_to(:blogger,
+    class_name: "User",
+    foreign_key: "blogger_id"
+  )
 end
